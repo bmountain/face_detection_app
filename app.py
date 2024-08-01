87a55ac5fe1e49bd95cad7212447322ffb934ec1
kona <ken5konan5@icloud.com>
2024-08-01 13:15:56 +0900

    add requirements.txt and app.py

--- app.py
+++ app.py
@@ -58,4 +58,5 @@ def video():
     return Response(gen(video), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__=="__main__":
-    app.run(host='0.0.0.0', port=8000)
+    port = int(os.environ.get('PORT', 5000))  # 環境変数 PORT を取得し、デフォルトは 5000
+    app.run(debug=True, host='0.0.0.0', port=port)
